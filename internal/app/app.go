c26e28443dbfbe56798a83beb5903508e5d7cd25
Vlad Vovk <wolfykey@gmail.com>
2025-05-11 23:03:39 +0200

    Implement database migrations using Goose
    - Added migration support in the application by integrating Goose for managing database migrations.
    - Created migration functions in the store package to handle file system-based migrations.
    - Updated the user, workouts, and workout_entries migration files to improve table definitions and constraints.
    - Changed the DROP TABLE statements to remove the "IF EXISTS" clause for consistency.

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/v-vovk/femProject/internal/api"
 	"github.com/v-vovk/femProject/internal/store"
+	"github.com/v-vovk/femProject/migrations"
 )
 
 type Application struct {
@@ -21,6 +22,11 @@ func New() (*Application, error) {
 	pgDB, err := store.Open()
 	if err != nil {
 		return nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
 	}
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
--- migrations/00001_users.sql
+++ migrations/00001_users.sql
@@ -1,17 +1,17 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS users (
-    id BIGSERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    bio TEXT,
-    created_at TIMESTAMP WITH TIMEZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIMEZONE DEFAULT CURRENT_TIMESTAMP,
-);
+  id BIGSERIAL PRIMARY KEY,
+  username VARCHAR(50) UNIQUE NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password_hash VARCHAR(255) NOT NULL,
+  bio TEXT,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+)
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS users;
+DROP TABLE users;
 -- +goose StatementEnd
